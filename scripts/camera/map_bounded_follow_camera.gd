256423ef18ac04a9e9fdde3d3c0833a3f0ec9a44
Max Goad <maxlgoad@gmail.com>
2024-07-11 16:15:15 +0900

    Added offset to map bounded follow camera

--- scripts/camera/map_bounded_follow_camera.gd
+++ scripts/camera/map_bounded_follow_camera.gd
@@ -10,10 +10,10 @@ class_name MapBoundedFollowCamera extends Camera2D
 ### Engine Functions
 func _ready() -> void:
 	var limit: Rect2 = bound.get_used_rect()
-	self.limit_left = int(limit.position.x * bound.cell_quadrant_size * bound.scale.x)
-	self.limit_top = int(limit.position.y * bound.cell_quadrant_size * bound.scale.y)
-	self.limit_right = int(limit.end.x * bound.cell_quadrant_size * bound.scale.x)
-	self.limit_bottom = int(limit.end.y * bound.cell_quadrant_size * bound.scale.y)
+	self.limit_left = int((limit.position.x * bound.cell_quadrant_size * bound.scale.x) + self.offset.x)
+	self.limit_top = int((limit.position.y * bound.cell_quadrant_size * bound.scale.y) - self.offset.y)
+	self.limit_right = int((limit.end.x * bound.cell_quadrant_size * bound.scale.x) + self.offset.x)
+	self.limit_bottom = int((limit.end.y * bound.cell_quadrant_size * bound.scale.y) - self.offset.y)
 
 func _process(_delta: float) -> void:
 	if following:
