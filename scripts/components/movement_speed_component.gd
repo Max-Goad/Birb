c46101274ac985d134f16a8460c71cfbf6b6bbe0
Max Goad <maxlgoad@gmail.com>
2024-07-14 21:14:53 +0900

    Fixed MovementSpeedComponent file location

--- scripts/components/movement_speed_component.gd
+++ scripts/components/movement_speed_component.gd
@@ -3,7 +3,3 @@ class_name MovementSpeedComponent extends Node
 @export var top_speed: float
 @export_range(0.01,1.0) var acceleration := 1.0
 @export_range(0.01,1.0) var deceleration := 1.0
-
-var top_speed_multiplier = 1.0
-var acceleration_multiplier = 1.0
-var deceleration_multiplier = 1.0
