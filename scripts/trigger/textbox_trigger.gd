49f6f7bb4cdb8ea39a30ad28f9473efc16223d46
Max Goad <maxlgoad@gmail.com>
2024-07-23 20:42:29 +0900

    Updated textbox to scroll text dynamically

--- scripts/trigger/textbox_trigger.gd
+++ scripts/trigger/textbox_trigger.gd
@@ -1,27 +1,30 @@
 class_name TextboxTrigger extends Trigger
 
 @export_multiline var text: String = ""
-@export_range(1.0, 10.0) var timeout: float = 5.0
+@export_range(0.0, 10.0) var timeout: float = 0.0
 @export var settings: LabelSettings
+@export var text_speed: float = 0.0
 @export var canvas: CanvasLayer = null
 
 const textbox_scene = preload("res://resources/ui/textbox.tscn")
 
 @warning_ignore("shadowed_variable")
-static func new_node(text = "", timeout = 5.0, settings = null) -> TextboxTrigger:
+static func new_node(text = "", timeout = 0.0, settings = null, text_speed = 0.0) -> TextboxTrigger:
 	var tt = Node.new()
 	tt.set_script(Trigger.Basic.textbox)
 	var textbox_trigger = tt as TextboxTrigger
 	textbox_trigger.text = text
 	textbox_trigger.timeout = timeout
 	textbox_trigger.settings = settings
+	textbox_trigger.text_speed = text_speed
 	return textbox_trigger
 
 func execute():
 	var textbox: Textbox = textbox_scene.instantiate()
 	textbox.text = text
-	textbox.timeout_override = timeout
+	textbox.timeout = timeout
 	textbox.settings = settings
+	textbox.text_speed = text_speed
 	if canvas == null:
 		get_tree().current_scene.add_child(textbox)
 	else:
--- scripts/ui/textbox.gd
+++ scripts/ui/textbox.gd
@@ -1,22 +1,39 @@
 class_name Textbox extends Control
 
 ### Variables
-@onready var label: Label = $PanelContainer/MarginContainer/Label
+@onready var label: Label = $PanelContainer/HBoxContainer/TextContainer/Label
+@onready var carat: Label = $PanelContainer/HBoxContainer/CaratContainer/Carat
 @onready var timer: Timer= $Timer
 
 @export_multiline var text: String
 @export var settings: LabelSettings
+@export var text_speed: float = 15.0
 @export var cancellable: bool = true
 @export var finished: bool = false
 
-@export var timeout_override: float
+@export var timeout: float
+
+var tween: Tween
 
 ### Signals
 signal complete
 
 ### Public Functions
 func start():
-	timer.start()
+	print("Textbox: start")
+	if text_speed > 0.0:
+		label.visible_ratio = 0.0
+		tween = create_tween()
+		tween.tween_property(label, "visible_ratio", 1.0, label.text.length() / text_speed)
+		tween.tween_callback(_display_carat)
+		if timeout:
+			tween.tween_callback(_start_despawn_timer)
+	else:
+		label.visible_ratio = 1.0
+		_display_carat()
+		if timeout:
+			_start_despawn_timer()
+
 
 func finish():
 	if not finished:
@@ -32,8 +49,9 @@ func _ready():
 		label.label_settings = settings
 	timer.one_shot = true
 	timer.timeout.connect(finish)
-	if timeout_override:
-		timer.wait_time = timeout_override
+	if timeout:
+		timer.wait_time = timeout
+	carat.visible_ratio = 0.0
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -44,3 +62,11 @@ func _gui_input(event: InputEvent) -> void:
 	if cancellable and event is InputEventMouseButton and event.button_index in [MOUSE_BUTTON_LEFT, MOUSE_BUTTON_RIGHT] and event.is_released():
 		print("Textbox: cancelled (mouse)")
 		finish()
+
+### Private Functions
+func _display_carat():
+	carat.visible_ratio = 1.0
+
+func _start_despawn_timer():
+	print("Textbox: start despawn timer")
+	timer.start()
