c59edcc6c9e77a0c6029e2c040bef3c63558d0e9
Max Goad <maxlgoad@gmail.com>
2024-07-26 09:21:41 +0900

    Added limit collisions flag to Hurtbox

--- scripts/ability/hurtbox.gd
+++ scripts/ability/hurtbox.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Hurtbox extends Area2D
 
 const MUTUAL_IGNORE = true
@@ -12,6 +13,10 @@ var angular_deceleration = 0.0
 var ignored_nodes: Dictionary = {}
 var collided_ids: Dictionary = {}
 
+@export var limit_collisions = true :
+	set(value):
+		limit_collisions = value
+		notify_property_list_changed()
 @export var max_collisions = 1
 @export var ignore_repeat_collisions = true
 @export var frame_alt_damage: Array[bool]
@@ -28,12 +33,16 @@ var freed_emitted = false
 
 ### Engine Functions
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
 	# assert(speed_component)
 	assert(damage_component)
 	body_shape_entered.connect(_hit_body)
 	area_shape_entered.connect(_hit_area)
 
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
 	if _check_raycast():
 		print("Hurtbox: raycast collision detected")
 		self.global_position = raycast.get_collision_point()
@@ -43,6 +52,10 @@ func _process(_delta: float) -> void:
 	# Dampen velocities
 	self.velocity = velocity.move_toward(Vector2.ZERO, velocity.length() * deceleration)
 	self.angular_velocity = move_toward(angular_velocity, 0.0, angular_deceleration)
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "max_collisions" and not limit_collisions:
+		property.usage &= ~PROPERTY_USAGE_EDITOR
 
 ### Public Functions
 func ignore(node: Node2D):
@@ -66,10 +79,12 @@ static func mutual_ignore(hurtboxes: Array[Hurtbox]):
 func clear_ignores():
 	self.ignored_nodes.clear()
 
-func reset_collisions(max_collisions = self.max_collisions):
+func reset_collisions(max_collisions = 0):
 	print("Hurtbox: reset collisions")
 	collided_ids.clear()
-	self.max_collisions = max_collisions
+	if max_collisions > 0:
+		self.limit_collisions = true
+		self.max_collisions = max_collisions
 	for body in get_overlapping_bodies():
 		_hit_body(body.get_rid(), body, 0, 0)
 	for area in get_overlapping_areas():
@@ -94,26 +109,32 @@ func despawn():
 		freed_emitted = true
 
 ### Private Functions
+func _should_ignore(rid) -> bool:
+	return ignore_repeat_collisions and collided_ids.has(rid)
+
+func _collision_limit_reached() -> bool:
+	return limit_collisions and collided_ids.size() >= max_collisions
+
 func _check_raycast() -> bool:
 	if not raycast:
 		return false
 	raycast.global_rotation = 0.0
 	raycast.target_position = self.velocity
 	raycast.force_raycast_update()
-	return raycast.is_colliding()
+	return raycast.is_colliding() and not _should_ignore(raycast.get_collider_rid())
 
 func _hit_body(body_rid: RID, body: Node2D, _body_shape_index: int, local_shape_index: int):
 	if finished_emitted or freed_emitted:
 		return
 	if body in ignored_nodes:
 		return
-	if ignore_repeat_collisions and collided_ids.has(body_rid) or collided_ids.size() > max_collisions:
+	if _should_ignore(body_rid) or _collision_limit_reached():
 		return
 	print("Hurtbox: hit body %s, alt? %s" % [local_shape_index,_index_is_alt(local_shape_index)])
 	var success = damage_component.apply(body, velocity, _index_is_alt(local_shape_index))
 	if success:
 		collided_ids[body_rid] = null
-		if collided_ids.size() >= max_collisions:
+		if _collision_limit_reached():
 			despawn()
 
 func _hit_area(area_rid: RID, area: Area2D, _area_shape_index: int, local_shape_index: int):
@@ -121,7 +142,7 @@ func _hit_area(area_rid: RID, area: Area2D, _area_shape_index: int, local_shape_
 		return
 	if area in ignored_nodes:
 		return
-	if ignore_repeat_collisions and collided_ids.has(area_rid) or collided_ids.size() > max_collisions:
+	if _should_ignore(area_rid) or _collision_limit_reached():
 		return
 
 	var success = false
@@ -133,7 +154,7 @@ func _hit_area(area_rid: RID, area: Area2D, _area_shape_index: int, local_shape_
 		success = damage_component.apply(area.get_parent(), velocity, _index_is_alt(local_shape_index))
 	if success:
 		collided_ids[area_rid] = null
-		if collided_ids.size() >= max_collisions:
+		if _collision_limit_reached():
 			despawn()
 
 func _index_is_alt(i) -> bool:
