deddecd6ff810dea38f492829b74d0da79f93a14
Max Goad <maxlgoad@gmail.com>
2024-07-26 22:36:05 +0900

    Added proper rotation handling to camera

--- scripts/camera/map_bounded_follow_camera.gd
+++ scripts/camera/map_bounded_follow_camera.gd
@@ -6,6 +6,7 @@ class_name MapBoundedFollowCamera extends Camera2D
 @export var following: bool = true
 
 var _last_rotation = 0
+var rot_num = 0
 #endregion
 
 #region Signals
@@ -13,21 +14,15 @@ var _last_rotation = 0
 
 #region Engine Functions
 func _ready() -> void:
-	var limit: Rect2 = bound.get_used_rect()
-	self.limit_left = int(limit.position.x * bound.cell_quadrant_size * bound.scale.x)
-	self.limit_top = int(limit.position.y * bound.cell_quadrant_size * bound.scale.y)
-	self.limit_right = int(limit.end.x * bound.cell_quadrant_size * bound.scale.x)
-	self.limit_bottom = int(limit.end.y * bound.cell_quadrant_size * bound.scale.y)
-	_apply_limit_offsets()
+	_update_rotation()
 	self.ignore_rotation = false
 	self.add_to_group(Data.GROUP_CAMERA)
 
 func _process(_delta: float) -> void:
 	if following:
 		var scene_camera = get_viewport().get_camera_2d()
-		if _rotation_changed(scene_camera):
-			_update_rotation(scene_camera)
-			#scene_camera.rotation = rotation
+		if _rotation_changed():
+			_update_rotation()
 		scene_camera.position = follow.position
 #endregion
 
@@ -41,29 +36,49 @@ func _apply_limit_offsets():
 	self.limit_right += int(self.offset.x)
 	self.limit_bottom -= int(self.offset.y)
 
-func _remove_limit_offsets():
-	self.limit_left -= int(self.offset.x)
-	self.limit_top +=  int(self.offset.y)
-	self.limit_right -= int(self.offset.x)
-	self.limit_bottom += int(self.offset.y)
-
-func _rotation_changed(camera):
+func _rotation_changed():
 	return rotation != _last_rotation
 
-func _update_rotation(camera):
-	_remove_limit_offsets()
-	var old_limit_left = self.limit_left
-	var old_limit_top = self.limit_top
-	var old_limit_right = self.limit_right
-	var old_limit_bottom = self.limit_bottom
-	self.limit_left = old_limit_bottom
-	self.limit_top = old_limit_left
-	self.limit_right = old_limit_top
-	self.limit_bottom = old_limit_right
-	self.offset = self.offset.rotated(rotation)
+func _update_rotation():
+	var limit: Rect2 = bound.get_used_rect()
+	var viewport: Rect2 = get_viewport_rect()
+	var x = int(limit.position.x)
+	var y = int(limit.position.y)
+	var w = int(limit.end.x)
+	var h = int(limit.end.y)
+	var cell = bound.rendering_quadrant_size
+	var sx = int(bound.scale.x)
+	var sy = int(bound.scale.y)
+	var vw = int(viewport.size.x)
+	var vh = int(viewport.size.y)
+
+	self.limit_left = x
+	self.limit_top = y
+	self.limit_right = (w*cell*sx) # 2560
+	self.limit_bottom = (h*cell*sy) # 1472
+	match rot_num:
+		0:
+			# Do nothing, you're perfect the way you are bb <3
+			pass
+		1:
+			self.limit_left += vh
+			self.limit_top += 0
+			self.limit_right += (vw - vh) + vh
+			self.limit_bottom += -(vw - vh)
+		2:
+			self.limit_left += vw
+			self.limit_top += vh
+			self.limit_right += vw
+			self.limit_bottom += vh
+		3:
+			self.limit_left += 0
+			self.limit_top += vw
+			self.limit_right += (vw - vh)
+			self.limit_bottom +=  -(vw - vh) + vw
+
 	_apply_limit_offsets()
-	# camera.rotation = rotation
 	_last_rotation = rotation
+	rot_num = (rot_num + 1) % 4
 
 
 #endregion
