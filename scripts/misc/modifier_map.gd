32c266d09857cd2e154e595c35413d90f25e46a9
Max Goad <maxlgoad@gmail.com>
2024-07-26 12:16:50 +0900

    Updated modifier map to take keys of any type (in anticipation of enums)

--- scripts/misc/modifier_map.gd
+++ scripts/misc/modifier_map.gd
@@ -8,21 +8,21 @@ var _map: Dictionary
 #endregion
 
 #region Public Functions
-func add(key: String, applier: Object, value: float):
+func add(key, applier: Object, value: float):
 	var value_pair = _map.get(key, {})
 	value_pair[applier] = value
 	_map[key] = value_pair
 
-func remove(key: String, applier: Object):
+func remove(key, applier: Object):
 	assert(key in _map)
 	assert(applier in _map[key])
 	_map[key].erase(applier)
 
 # Fuck it I have a headache and no function override is stupid AF
-func gett(key: String) -> float:
+func gett(key) -> float:
 	return _map.get(key, {}).values().reduce(func(a,b): return a*b, 1.0)
 
-func clear(key: String):
+func clear(key):
 	assert(key in _map)
 	_map[key].clear()
 
