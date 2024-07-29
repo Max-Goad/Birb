ed5c01abb8016173b5f5435a61e1e484e767ca99
Max Goad <maxlgoad@gmail.com>
2024-07-29 15:16:25 +0900

    Updated Prisoner ability

--- scripts/ability/ability_prisoner.gd
+++ scripts/ability/ability_prisoner.gd
@@ -1,6 +1,7 @@
 class_name Prisoner extends Ability
 
 const PL_TARGET_RETICLE = preload("res://resources/ui/target_reticle.tscn")
+const PRISONER_SPRITE = preload("res://resources/attacks/prisoner_sprite.tscn")
 
 #region Variables
 var effect_time: float
@@ -23,9 +24,10 @@ func _init(effect_time) -> void:
 #region Public Functions
 func execute(parent: Player, direction: Vector2):
 	super.execute(parent, direction)
-	var target = _closest_enemy_center()
-	if target:
-		target.get_parent().modulate = Color(Color.RED, 0.5)
+	var target: Enemy = _closest_enemy()
+	if target and not target.movement.currently_locked:
+		_lock_movement(target)
+		_attach_sprite(target)
 	finished.emit()
 
 func on_set():
@@ -38,10 +40,27 @@ func on_unset():
 #endregion
 
 #region Private Functions
+func _closest_enemy() -> Enemy:
+	return Data.get_closest_enemy(Data.get_player().global_position)
+
 func _closest_enemy_center() -> Node2D:
-	var closest_enemy: Enemy = Data.get_closest_enemy(Data.get_player().global_position)
+	var closest_enemy: Enemy = _closest_enemy()
 	if closest_enemy:
 		return closest_enemy.attach_points.center
 	else:
 		return null
+
+func _lock_movement(target: Enemy):
+	var lock_movement_trigger = LockMovementTrigger.new()
+	lock_movement_trigger.movement = target.movement
+	lock_movement_trigger.time = effect_time
+	target.add_child(lock_movement_trigger)
+	lock_movement_trigger.execute()
+
+func _attach_sprite(target: Enemy):
+	var sprite = PRISONER_SPRITE.instantiate()
+	target.attach_points.center.add_child(sprite)
+	var detach_sprite = func(): sprite.queue_free()
+	target.movement.unlocked.connect(detach_sprite)
+
 #endregion
