ad8e7bb31d162e134a86b712e88c41d6ff8a5ade
Max Goad <maxlgoad@gmail.com>
2024-07-30 21:47:56 +0900

    Updated pathfinding track strategy

--- scripts/components/pathfinding_component.gd
+++ scripts/components/pathfinding_component.gd
@@ -8,6 +8,13 @@ enum Strategy
 	TRACK_FROM_DISTANCE,
 }
 
+enum TrackState
+{
+	IN_RANGE,
+	FAR,
+	CLOSE,
+}
+
 #region Variables
 @export var strategy = Strategy.FOLLOW:
 	set(value):
@@ -20,6 +27,8 @@ enum Strategy
 # Used in Strategy.TRACK_FROM_DISTANCE
 @export var distance: float = 100.0
 var distance_margin = 50.0
+
+var current_track_state: TrackState
 #endregion
 
 #region Signals
@@ -53,23 +62,28 @@ func _process_strategy(_delta):
 				movement.apply_velocity(_get_vector_moving_towards(target))
 		Strategy.TRACK_FROM_DISTANCE:
 			if target:
+				if true:
+					movement.apply_rotation(movement.character.global_rotation + 0.1)
+					return
+				var direction_to_target = _get_vector_to_target(target)
 				var target_speed = movement.top_speed
 				var magnitude = move_toward(movement.character.velocity.length(), target_speed, target_speed * movement.acceleration)
-				var vector = _get_vector_to_target(target) * magnitude
-				var perpendicular = vector.rotated(PI/2)
-				movement.apply_rotation(vector.angle() + PI/2)
-				var gap = movement.character.global_position.distance_to(target.global_position)
-				if gap > distance + distance_margin:
-					print("gap >")
-					var x = vector + perpendicular
-					movement.apply_velocity(x)
-				elif gap < distance - distance_margin:
-					print("gap <")
-					var x = -vector + perpendicular
-					movement.apply_velocity(x)
-				else:
-					print("gap within")
-					movement.apply_velocity(perpendicular)
+
+				current_track_state = _update_track_state()
+				match current_track_state:
+					TrackState.FAR:
+						# move towards the target
+						movement.apply_velocity(direction_to_target * magnitude)
+					TrackState.IN_RANGE:
+						# circle the target
+						var perpendicular = direction_to_target.rotated(PI/2)
+						movement.move_velocity_toward(perpendicular * magnitude, target_speed * movement.acceleration)
+					TrackState.CLOSE:
+						# move away from the target
+						movement.apply_velocity(-direction_to_target * magnitude)
+
+				# always face the target
+				movement.apply_rotation(direction_to_target.angle() + PI/2)
 
 func _get_vector_moving_towards(target) -> Vector2:
 	var character = movement.character
@@ -85,4 +99,25 @@ func _get_vector_moving_towards(target) -> Vector2:
 
 func _get_vector_to_target(target) -> Vector2:
 	return (target.global_position - movement.character.global_position).normalized()
+
+func _update_track_state() -> TrackState:
+	var gap = movement.character.global_position.distance_to(target.global_position)
+	match current_track_state:
+		TrackState.FAR when gap > distance:
+			return TrackState.FAR
+		TrackState.FAR:
+			return TrackState.IN_RANGE
+		TrackState.IN_RANGE when gap > distance + distance_margin:
+			return TrackState.FAR
+		TrackState.IN_RANGE when gap < distance - distance_margin:
+			return TrackState.CLOSE
+		TrackState.IN_RANGE:
+			return TrackState.IN_RANGE
+		TrackState.CLOSE when gap < distance:
+			return TrackState.CLOSE
+		TrackState.CLOSE:
+			return TrackState.IN_RANGE
+		_:
+			assert(false)
+			return TrackState.IN_RANGE
 #endregion
