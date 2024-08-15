dc822c1506808e1a113ebd3331f3fe1993c5da28
Max Goad <maxlgoad@gmail.com>
2024-08-15 15:05:04 +0900

    Cleaned up map transitions

--- scripts/maps/map_transition.gd
+++ scripts/maps/map_transition.gd
@@ -1,9 +1,10 @@
+@tool
 class_name MapTransition extends Area2D
 
 #region Variables
 @export var id: int
-@export var target_map_id: int
-@export var target_transition_id: int
+@export var target_map: String
+@export var target_id: int
 
 @onready var entry: Node2D = $Entry
 @onready var exit: CollisionShape2D = $Exit
@@ -15,8 +16,18 @@ signal triggered(target_map_id, target_transition_id)
 
 #region Engine Functions
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		notify_property_list_changed()
+		return
+	assert(entry)
+	assert(exit)
 	area_entered.connect(_on_entered)
 	body_entered.connect(_on_entered)
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "target_map":
+		property.hint = PROPERTY_HINT_ENUM
+		property.hint_string = ",".join(Gameplay.get_map_names())
 #endregion
 
 #region Public Functions
@@ -25,6 +36,6 @@ func _ready() -> void:
 #region Private Functions
 func _on_entered(node):
 	if node is Player:
-		triggered.emit(target_map_id, target_transition_id)
+		triggered.emit(target_map, target_id)
 #endregion
 
--- scripts/scenes/gameplay_scene.gd
+++ scripts/scenes/gameplay_scene.gd
@@ -1,11 +1,14 @@
-extends Node2D
+class_name Gameplay extends Node2D
+
+const MAP_DIR = "res://resources/scenes/maps"
 
 #region Variables
 @onready var player: Player = $Player
 @onready var camera: MapBoundedFollowCamera = $Camera
 @onready var ui_canvas: CanvasLayer = $"UI Canvas"
 
-@export var maps: Array[PackedScene]
+# String (filename) : PackedScene
+var maps: Dictionary
 var current_map: Map
 #endregion
 
@@ -14,33 +17,40 @@ var current_map: Map
 
 #region Engine Functions
 func _ready() -> void:
+	_load_all_maps(Gameplay.MAP_DIR)
 	if not current_map:
-		current_map = _load_map(0)
+		current_map = _load_map(Gameplay.get_map_names()[0])
 
 func _process(_delta: float) -> void:
 	pass
 #endregion
 
 #region Public Functions
+static func get_map_names() -> PackedStringArray:
+	return DirAccess.get_files_at(Gameplay.MAP_DIR)
 #endregion
 
 #region Private Functions
-func _on_map_transition_trigger(map_id: int, transition_id: int):
-	_on_map_transition.call_deferred(map_id, transition_id)
+func _on_map_transition_trigger(map_name: String, transition_id: int):
+	_on_map_transition.call_deferred(map_name, transition_id)
 
-func _on_map_transition(map_id: int, transition_id: int):
+func _on_map_transition(map_name: String, transition_id: int):
 	# Load new map
 	var old_map = current_map
-	current_map = _load_map(map_id)
+	current_map = _load_map(map_name)
 	# Move player to target id location
 	var new_transition = current_map.get_transition(transition_id)
 	player.global_position = new_transition.entry.global_position
 	# Unload old map
 	old_map.queue_free()
 
-func _load_map(id) -> Map:
-	assert(id >= 0 and id < maps.size())
-	var new_map: Map = maps[id].instantiate()
+func _load_all_maps(dir_path: String):
+	for path in DirAccess.get_files_at(dir_path):
+		maps[path] = (load("%s/%s" % [dir_path,path]))
+
+func _load_map(map_name) -> Map:
+	assert(map_name in maps)
+	var new_map: Map = maps[map_name].instantiate()
 	new_map.enter()
 	new_map.transition_triggered.connect(_on_map_transition_trigger)
 	add_child(new_map)
