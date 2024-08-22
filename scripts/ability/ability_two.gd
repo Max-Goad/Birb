0b1929114f73c34371bd4d1dd66581245c23d7b9
Max Goad <maxlgoad@gmail.com>
2024-08-22 14:40:21 +0900

    Updated Two ability to use chains

--- scripts/ability/ability_two.gd
+++ scripts/ability/ability_two.gd
@@ -31,29 +31,28 @@ func _init(speed, damage, delay) -> void:
 #region Public Functions
 func execute(parent: Player, direction: Vector2):
 	super.execute(parent, direction)
-	var execute_fn = func():
-		_fire_projectile(parent, direction)
-		_projectile_delay_timer.timeout.connect(_fire_second_projectile.bind(parent, direction))
-		_projectile_delay_timer.start(delay)
-	super.execute_with_delay(execute_fn, 0.1)
+	(super.chain()
+		.wait(0.1)
+		.run(_fire_projectile)
+		.wait(delay)
+		.run(_fire_projectile)
+		.wait(0.2)
+		.run(self.finish)
+		.start_chain()
+	)
 #endregion
 
 #region Private Functions
-func _fire_projectile(parent: Player, direction: Vector2, callback = func():pass):
+func _fire_projectile():
 	var hurtbox: Hurtbox = projecile_template.instantiate()
 	hurtbox.top_level = true # Do not follow parent's transforms
-	hurtbox.position = parent.position
-	hurtbox.scale = parent.scale
-	var clamped_direction = Math.vector8dir(direction) * Math.dither_f(self.speed, 5)
+	hurtbox.position = self.parent.position
+	hurtbox.scale = self.parent.scale
+	var clamped_direction = Math.vector8dir(self.direction) * Math.dither_f(self.speed, 5)
 	var dithered_direction = Math.dither_v_rot(clamped_direction, deg_to_rad(10))
 	hurtbox.velocity = dithered_direction
 	hurtbox.rotate(dithered_direction.angle())
-	hurtbox.finished.connect(callback)
-	hurtbox.ignore(parent)
-	parent.add_child(hurtbox)
-	hurtbox.damage_component.amount = int(damage * parent.modifiers.gett(Player.Modifiers.DAMAGE))
-
-func _fire_second_projectile(parent: Node2D, direction: Vector2):
-	_fire_projectile(parent, direction, func(): self.finished.emit())
-	_projectile_delay_timer.timeout.disconnect(_fire_second_projectile.bind(parent, direction))
+	hurtbox.ignore(self.parent)
+	self.parent.add_child(hurtbox)
+	hurtbox.damage_component.amount = int(damage * self.parent.modifiers.gett(Player.Modifiers.DAMAGE))
 #endregion
