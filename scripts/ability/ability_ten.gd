34ee5d8f95217e0e5b482443074f054a408e1211
Max Goad <maxlgoad@gmail.com>
2024-08-22 15:19:12 +0900

    Fixed Ten implementation

--- scripts/ability/ability_ten.gd
+++ scripts/ability/ability_ten.gd
@@ -11,10 +11,6 @@ var impact_damage: int
 var explosion_damage: int
 var projectile_damage: int
 var explosion_timer: float
-
-var current_parent: Node2D = null
-var current_bomb: Hurtbox = null
-var current_explosion: Hurtbox = null
 #endregion
 
 #region Signals
@@ -36,10 +32,7 @@ func _init(tf, pf, id, ed, pd, et = 1.0) -> void:
 
 #region Public Functions
 func execute(parent: Player, direction: Vector2):
-	assert(current_bomb == null)
-	assert(current_explosion == null)
 	super.execute(parent, direction)
-	self.current_parent = parent
 	(self.chain()
 		.wait(0.1)
 		.run(_throw_bomb)
@@ -50,58 +43,54 @@ func execute(parent: Player, direction: Vector2):
 
 #region Private Functions
 func _throw_bomb():
-	self.current_bomb = pl_hb_ten_bomb.instantiate()
-	current_bomb.top_level = true # Do not follow parent's transforms
-	current_bomb.position = current_parent.position
-	current_bomb.scale = current_parent.scale
-	current_bomb.velocity = Math.dither_v_rot(Math.vector8dir(self.direction) * Math.dither_f(throw_force, 5), deg_to_rad(10))
-	current_bomb.deceleration = 0.15
-	current_bomb.angular_velocity = Math.rand_negative(Math.dither_f(10.0, 5.0))
-	current_bomb.angular_deceleration = Math.dither_f(0.3, 0.15)
-	current_bomb.ignore(current_parent)
-	current_bomb.finished.connect(func(): call_deferred("_on_bomb_finished"))
-	current_parent.add_child(current_bomb)
-	current_bomb.damage_component.amount = int(impact_damage * current_parent.modifiers.gett(Player.Modifiers.DAMAGE))
+	var bomb = pl_hb_ten_bomb.instantiate()
+	bomb.top_level = true # Do not follow parent's transforms
+	bomb.position = self.parent.position
+	bomb.scale = self.parent.scale
+	bomb.velocity = Math.dither_v_rot(Math.vector8dir(self.direction) * Math.dither_f(throw_force, 5), deg_to_rad(10))
+	bomb.deceleration = 0.15
+	bomb.angular_velocity = Math.rand_negative(Math.dither_f(10.0, 5.0))
+	bomb.angular_deceleration = Math.dither_f(0.3, 0.15)
+	bomb.ignore(self.parent)
+	bomb.finished.connect(func(): call_deferred("_on_bomb_finished", bomb))
+	self.parent.add_child(bomb)
+	bomb.damage_component.amount = int(impact_damage * self.parent.modifiers.gett(Player.Modifiers.DAMAGE))
 
-func _on_bomb_finished():
-	_spawn_explosion()
-	_spawn_all_projectiles()
-	current_bomb = null
+func _on_bomb_finished(bomb):
+	var explosion = _spawn_explosion(bomb)
+	_spawn_all_projectiles(bomb, explosion)
 
-func _spawn_explosion():
-	self.current_explosion = pl_hb_ten_explosion.instantiate()
-	current_explosion.top_level = true # Do not follow parent's transforms
-	current_explosion.position = current_bomb.position
-	current_explosion.scale = current_bomb.scale
-	# current_explosion.ignore(current_parent)
-	current_explosion.finished.connect(_on_explosion_finished)
-	current_parent.add_child(current_explosion)
-	current_explosion.damage_component.amount = int(explosion_damage * current_parent.modifiers.gett(Player.Modifiers.DAMAGE))
+func _spawn_explosion(bomb) -> Hurtbox:
+	var explosion = pl_hb_ten_explosion.instantiate()
+	explosion.top_level = true # Do not follow parent's transforms
+	explosion.position = bomb.position
+	explosion.scale = bomb.scale
+	# explosion.ignore(self.parent)
+	self.parent.add_child(explosion)
+	explosion.damage_component.amount = int(explosion_damage * self.parent.modifiers.gett(Player.Modifiers.DAMAGE))
 
 	# There's a small amount of time when both are still alive/active
-	current_explosion.ignore_hurtbox(current_bomb, Hurtbox.MUTUAL_IGNORE)
+	explosion.ignore_hurtbox(bomb, Hurtbox.MUTUAL_IGNORE)
+	return explosion
 
-func _on_explosion_finished():
-	current_explosion = null
-
-func _spawn_all_projectiles():
+func _spawn_all_projectiles(bomb, explosion):
 	var spawned_projectiles: Array[Hurtbox] = []
 	for added_rotation in [0, 90, 180, 270]:
 		var projectile = pl_hb_ten_projectile.instantiate()
 		projectile.name = "Ten Projectile %s" % added_rotation
 		projectile.top_level = true # Do not follow parent's transforms
-		projectile.position = current_bomb.position
-		projectile.scale = current_bomb.scale
-		projectile.rotation_degrees = current_bomb.rotation_degrees + added_rotation
+		projectile.position = bomb.position
+		projectile.scale = bomb.scale
+		projectile.rotation_degrees = bomb.rotation_degrees + added_rotation
 		projectile.velocity = Vector2.RIGHT.rotated(projectile.rotation) * projectile_force
-		current_parent.add_child(projectile)
-		projectile.damage_component.amount = int(projectile_damage * current_parent.modifiers.gett(Player.Modifiers.DAMAGE))
+		self.parent.add_child(projectile)
+		projectile.damage_component.amount = int(projectile_damage * self.parent.modifiers.gett(Player.Modifiers.DAMAGE))
 		spawned_projectiles.append(projectile)
 
 	# Projectiles, Bomb, and Explosion should ignore each other
 	for p in spawned_projectiles:
-		p.ignore_hurtbox(current_bomb, Hurtbox.MUTUAL_IGNORE)
-		p.ignore_hurtbox(current_explosion, Hurtbox.MUTUAL_IGNORE)
+		p.ignore_hurtbox(bomb, Hurtbox.MUTUAL_IGNORE)
+		p.ignore_hurtbox(explosion, Hurtbox.MUTUAL_IGNORE)
 	# Projectiles should ignore each other
 	Hurtbox.mutual_ignore(spawned_projectiles)
 #endregion
